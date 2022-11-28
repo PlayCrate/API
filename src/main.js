37df16e4789c9f6153535966f9824f1bb61d5d33
Kattah <kattahpro@gmail.com>
2022-11-28 19:51:29 +0900

    Updated yml

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ async function execute() {
   console.log(`Executing Roblox API...`);
   try {
     await sql.query(
-      `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username TEXT NOT NULL, uid TEXT NOT NULL, robloxID TEXT NOT NULL )`
+      `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username TEXT NOT NULL, twitter_id TEXT NOT NULL, robloxID TEXT NOT NULL, follow_date TIMESTAMP NOT NULL DEFAULT NOW(), game_id TEXT NOT NULL, twitter_account TEXT NOT NULL)`
     );
   } catch (err) {
     console.log(err);
