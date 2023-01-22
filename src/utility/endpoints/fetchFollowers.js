ef4fa3afea45e050e8cc413cc6325d8459044917
Kattah <kattahpro@gmail.com>
2023-01-23 04:17:08 +0900

    return id

--- src/utility/endpoints/fetchFollowers.js
+++ src/utility/endpoints/fetchFollowers.js
@@ -18,6 +18,7 @@ async function FetchAllFollowers() {
     const idsArray = data.ids;
     for (let i = 0; i < idsArray.length; i++) {
         const id = idsArray[i];
+        console.log(id);
         const check = await sql.query(`SELECT * FROM twitter_ids WHERE twitter_id = $1`, [id]);
         if (check.rows.length === 0) {
             await sql.query(`INSERT INTO twitter_ids (twitter_id) VALUES ($1)`, [id]);
