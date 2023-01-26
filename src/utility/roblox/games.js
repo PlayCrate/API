e90c581031d63216dc133314916c7611e82c27e9
Kattah <kattahpro@gmail.com>
2023-01-26 12:18:26 +0900

    Catch errors

--- src/utility/roblox/games.js
+++ src/utility/roblox/games.js
@@ -1,33 +1,32 @@
 const got = require('got');
 
 async function makeRequest(url) {
-    const { body } = await got(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'Roblox/WinInet',
-            'X-Requested-With': 'XMLHttpRequest',
-        },
-    });
+    try {
+        const { body } = await got(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'User-Agent': 'Roblox/WinInet',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+        });
 
-    if (!body) return;
-    const parsed = JSON.parse(body);
-    return parsed;
-}
-
-function Sleep() {
-    return new Promise((resolve) => setTimeout(resolve, 1000));
+        if (!body) return;
+        const parsed = JSON.parse(body);
+        return parsed;
+    } catch (err) {
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        return makeRequest(url);
+    }
 }
 
 exports.gameInfo = async (gameID) => {
-    await Sleep();
     const gameInfo = await makeRequest(`https://games.roblox.com/v1/games?universeIds=${gameID}`);
     const { playing, visits, favoritedCount } = gameInfo.data[0];
     return { playing, visits, favoritedCount };
 };
 
 exports.gameVotesInfo = async (gameID) => {
-    await Sleep();
     const votesInfo = await makeRequest(`https://games.roblox.com/v1/games/votes?universeIds=${gameID}`);
     const { upVotes, downVotes } = votesInfo.data[0];
     const ratings = upVotes + downVotes === 0 ? 0 : (upVotes / (upVotes + downVotes)) * 100;
@@ -36,7 +35,6 @@ exports.gameVotesInfo = async (gameID) => {
 };
 
 exports.groupInfo = async (groupID) => {
-    await Sleep();
     const groupInfo = await makeRequest(`https://groups.roblox.com/v1/groups/${groupID}`);
     return {
         memberCount: groupInfo.memberCount,
