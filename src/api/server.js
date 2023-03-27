ac8c8670ca9580eaa76625784468b29684c13fa3
Kattah <kattahpro@gmail.com>
2023-03-28 05:27:03 +0900

    removed cluster

--- src/api/server.js
+++ src/api/server.js
@@ -3,45 +3,24 @@ const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const cluster = require('cluster');
 
-if (cluster.isMaster) {
-    const numCPUs = require('os').cpus();
+const Routes = join(__dirname, 'Routes');
+const App = express();
 
-    console.log(`Master process started with PID ${process.pid} and ${numCPUs} CPUs`);
+App.use(cors(), express.json(), morgan('dev'));
+App.listen(bot.Config.server.port, () => {
+    console.log(`Server is running on port ${bot.Config.server.port}`);
+});
 
-    for (let i = 0; i < numCPUs.length; i++) {
-        cluster.fork();
-    }
+(async () => {
+    for (const file of fs.readdirSync(Routes)) {
+        const subFolder = join(Routes, file);
+        const subFolderFiles = fs.readdirSync(subFolder);
 
-    cluster.on('online', (worker) => {
-        console.log(`Worker ${worker.process.pid} is online`);
-    });
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
-        console.log('Starting a new worker');
-        cluster.fork();
-    });
-} else {
-    const Routes = join(__dirname, 'Routes');
-    const App = express();
-
-    App.use(cors(), express.json(), morgan('dev'));
-    App.listen(bot.Config.server.port, () => {
-        console.log(`Server is running on port ${bot.Config.server.port}`);
-    });
-
-    (async () => {
-        for (const file of fs.readdirSync(Routes)) {
-            const subFolder = join(Routes, file);
-            const subFolderFiles = fs.readdirSync(subFolder);
-
-            for (const subFile of subFolderFiles) {
-                if (!subFile.endsWith('.js')) continue;
-                const Route = require(join(subFolder, subFile));
-                App.use(Route);
-            }
+        for (const subFile of subFolderFiles) {
+            if (!subFile.endsWith('.js')) continue;
+            const Route = require(join(subFolder, subFile));
+            App.use(Route);
         }
-    })();
-}
+    }
+})();
