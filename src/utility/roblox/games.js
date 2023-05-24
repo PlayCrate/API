b46c83f251caebf97ce1817593f47a70e49d16b8
Kattah <kattahpro@gmail.com>
2023-05-24 23:52:18 +0900

    Removed grafana

--- src/utility/roblox/games.js
+++ src/utility/roblox/games.js
@@ -1,43 +1,43 @@
-const fetch = require('node-fetch');
-const randUserAgent = require('rand-user-agent');
-const agent = randUserAgent('desktop');
+// const fetch = require('node-fetch');
+// const randUserAgent = require('rand-user-agent');
+// const agent = randUserAgent('desktop');
 
-async function makeRequest(url) {
-    try {
-        const asd = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'User-Agent': agent,
-                'X-Requested-With': 'XMLHttpRequest',
-                'Accept': 'application/json',
-            },
-        }).then((res) => res.json());
-        if (asd.errors) throw new Error(asd.errors[0].message);
-        return asd;
-    } catch (err) {
-        throw new Error(err);
-    }
-}
+// async function makeRequest(url) {
+//     try {
+//         const asd = await fetch(url, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'User-Agent': agent,
+//                 'X-Requested-With': 'XMLHttpRequest',
+//                 'Accept': 'application/json',
+//             },
+//         }).then((res) => res.json());
+//         if (asd.errors) throw new Error(asd.errors[0].message);
+//         return asd;
+//     } catch (err) {
+//         throw new Error(err);
+//     }
+// }
 
-exports.gameInfo = async (gameID) => {
-    const gameInfo = await makeRequest(`https://games.roblox.com/v1/games?universeIds=${gameID}`);
-    const { playing, visits, favoritedCount } = gameInfo.data[0];
-    return { playing, visits, favoritedCount };
-};
+// exports.gameInfo = async (gameID) => {
+//     const gameInfo = await makeRequest(`https://games.roblox.com/v1/games?universeIds=${gameID}`);
+//     const { playing, visits, favoritedCount } = gameInfo.data[0];
+//     return { playing, visits, favoritedCount };
+// };
 
-exports.gameVotesInfo = async (gameID) => {
-    const votesInfo = await makeRequest(`https://games.roblox.com/v1/games/votes?universeIds=${gameID}`);
-    const { upVotes, downVotes } = votesInfo.data[0];
-    const ratings = upVotes + downVotes === 0 ? 0 : (upVotes / (upVotes + downVotes)) * 100;
-    const fixedRatings = ratings.toFixed(2);
-    return { upVotes, downVotes, fixedRatings };
-};
+// exports.gameVotesInfo = async (gameID) => {
+//     const votesInfo = await makeRequest(`https://games.roblox.com/v1/games/votes?universeIds=${gameID}`);
+//     const { upVotes, downVotes } = votesInfo.data[0];
+//     const ratings = upVotes + downVotes === 0 ? 0 : (upVotes / (upVotes + downVotes)) * 100;
+//     const fixedRatings = ratings.toFixed(2);
+//     return { upVotes, downVotes, fixedRatings };
+// };
 
-exports.groupInfo = async (groupID) => {
-    const groupInfo = await makeRequest(`https://groups.roblox.com/v1/groups/${groupID}`);
-    return {
-        name: groupInfo.name,
-        memberCount: groupInfo.memberCount,
-    };
-};
+// exports.groupInfo = async (groupID) => {
+//     const groupInfo = await makeRequest(`https://groups.roblox.com/v1/groups/${groupID}`);
+//     return {
+//         name: groupInfo.name,
+//         memberCount: groupInfo.memberCount,
+//     };
+// };
--- src/utility/roblox/register.js
+++ src/utility/roblox/register.js
@@ -1,197 +1,197 @@
-const prom = require('prom-client');
-const register = new prom.Registry();
-const Redis = require('ioredis');
-const redis = new Redis({});
-const sql = require('../../database/db');
+// const prom = require('prom-client');
+// const register = new prom.Registry();
+// const Redis = require('ioredis');
+// const redis = new Redis({});
+// const sql = require('../../database/db');
 
-async function returnSQL(type) {
-    const { rows } = await sql.query(`SELECT * FROM ROBUX WHERE purchase_type = '${type}';`);
-    let robuxSpent = 0;
-    for (const { robux_spent } of rows) {
-        robuxSpent += robux_spent;
-    }
+// async function returnSQL(type) {
+//     const { rows } = await sql.query(`SELECT * FROM ROBUX WHERE purchase_type = '${type}';`);
+//     let robuxSpent = 0;
+//     for (const { robux_spent } of rows) {
+//         robuxSpent += robux_spent;
+//     }
 
-    return robuxSpent;
-}
+//     return robuxSpent;
+// }
 
-const {
-    gamesCurrentUser: islandUser,
-    gamesCurrentVisits: islandVisit,
-    gamesCurrentFavorites: islandFav,
-    gameRating: islandRating,
-} = require('./places/island_life');
+// const {
+//     gamesCurrentUser: islandUser,
+//     gamesCurrentVisits: islandVisit,
+//     gamesCurrentFavorites: islandFav,
+//     gameRating: islandRating,
+// } = require('./places/island_life');
 
-const {
-    gamesCurrentUser: rotopiaUsers,
-    gamesCurrentVisits: rotopiaVisits,
-    gamesCurrentFavorites: rotopiaFav,
-    gameRating: rotopiaRating,
-} = require('./places/rotopia');
+// const {
+//     gamesCurrentUser: rotopiaUsers,
+//     gamesCurrentVisits: rotopiaVisits,
+//     gamesCurrentFavorites: rotopiaFav,
+//     gameRating: rotopiaRating,
+// } = require('./places/rotopia');
 
-const {
-    gamesCurrentUser: eatingUsers,
-    gamesCurrentVisits: eatingVisits,
-    gamesCurrentFavorites: eatingFav,
-    gameRating: eatingRating,
-} = require('./places/eating_sim');
+// const {
+//     gamesCurrentUser: eatingUsers,
+//     gamesCurrentVisits: eatingVisits,
+//     gamesCurrentFavorites: eatingFav,
+//     gameRating: eatingRating,
+// } = require('./places/eating_sim');
 
-const {
-    gamesCurrentUser: bubbleUsers,
-    gamesCurrentVisits: bubbleVisits,
-    gamesCurrentFavorites: bubbleFav,
-    gameRating: bubbleRating,
-    productRobux,
-    gamepassRobux,
-} = require('./places/bubble');
+// const {
+//     gamesCurrentUser: bubbleUsers,
+//     gamesCurrentVisits: bubbleVisits,
+//     gamesCurrentFavorites: bubbleFav,
+//     gameRating: bubbleRating,
+//     productRobux,
+//     gamepassRobux,
+// } = require('./places/bubble');
 
-const { PlayCrateGroupCount, MineCartGroupCount, BreadedGroupCount, StormyGroupCount } = require('./groups/groupInfo');
-const { CPUUsage, MemoryUsage } = require('./performance');
+// const { PlayCrateGroupCount, MineCartGroupCount, BreadedGroupCount, StormyGroupCount } = require('./groups/groupInfo');
+// const { CPUUsage, MemoryUsage } = require('./performance');
 
-const { groupInfo, gameInfo, gameVotesInfo } = require('./games');
+// const { groupInfo, gameInfo, gameVotesInfo } = require('./games');
 
-async function Looper() {
-    const groupIDs = ['13004189', '5799338', '3409253', '5998745'];
-    const promises = groupIDs.map(async (id) => {
-        const info = await groupInfo(id);
-        return { id, info };
-    });
+// async function Looper() {
+//     const groupIDs = ['13004189', '5799338', '3409253', '5998745'];
+//     const promises = groupIDs.map(async (id) => {
+//         const info = await groupInfo(id);
+//         return { id, info };
+//     });
 
-    const result = await Promise.all(promises);
-    for (const { id, info } of result) {
-        switch (id) {
-            case '13004189':
-                PlayCrateGroupCount.set(info.memberCount);
-                await redis.set('play_crate_group_count', info.memberCount);
-                break;
-            case '5799338':
-                MineCartGroupCount.set(info.memberCount);
-                break;
-            case '3409253':
-                BreadedGroupCount.set(info.memberCount);
-                break;
-            case '5998745':
-                StormyGroupCount.set(info.memberCount);
-                break;
-            default:
-                break;
-        }
-    }
+//     const result = await Promise.all(promises);
+//     for (const { id, info } of result) {
+//         switch (id) {
+//             case '13004189':
+//                 PlayCrateGroupCount.set(info.memberCount);
+//                 await redis.set('play_crate_group_count', info.memberCount);
+//                 break;
+//             case '5799338':
+//                 MineCartGroupCount.set(info.memberCount);
+//                 break;
+//             case '3409253':
+//                 BreadedGroupCount.set(info.memberCount);
+//                 break;
+//             case '5998745':
+//                 StormyGroupCount.set(info.memberCount);
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
 
-    const universeID = ['4158951932', '701618141', '1730143810', '3478025530'];
-    const promises2 = universeID.map(async (id) => {
-        const info = await gameInfo(id);
-        const votes = await gameVotesInfo(id);
-        return { id, info, votes };
-    });
+//     const universeID = ['4158951932', '701618141', '1730143810', '3478025530'];
+//     const promises2 = universeID.map(async (id) => {
+//         const info = await gameInfo(id);
+//         const votes = await gameVotesInfo(id);
+//         return { id, info, votes };
+//     });
 
-    const result2 = await Promise.all(promises2);
-    for (const { id, info, votes } of result2) {
-        const { playing, visits, favoritedCount } = info;
-        let { fixedRatings } = votes;
-        fixedRatings = Number(fixedRatings);
+//     const result2 = await Promise.all(promises2);
+//     for (const { id, info, votes } of result2) {
+//         const { playing, visits, favoritedCount } = info;
+//         let { fixedRatings } = votes;
+//         fixedRatings = Number(fixedRatings);
 
-        switch (id) {
-            case '4158951932':
-                bubbleUsers.set(playing);
-                bubbleVisits.set(visits);
-                bubbleFav.set(favoritedCount);
-                bubbleRating.set(fixedRatings);
+//         switch (id) {
+//             case '4158951932':
+//                 bubbleUsers.set(playing);
+//                 bubbleVisits.set(visits);
+//                 bubbleFav.set(favoritedCount);
+//                 bubbleRating.set(fixedRatings);
 
-                await redis.set('play_crate_playing', playing);
-                await redis.set('play_crate_visits', visits);
-                productRobux.set(await returnSQL('product'));
-                gamepassRobux.set(await returnSQL('gamepass'));
-                break;
-            case '701618141':
-                eatingUsers.set(playing);
-                eatingVisits.set(visits);
-                eatingFav.set(favoritedCount);
-                eatingRating.set(fixedRatings);
-                break;
-            case '1730143810':
-                islandUser.set(playing);
-                islandVisit.set(visits);
-                islandFav.set(favoritedCount);
-                islandRating.set(fixedRatings);
-                break;
-            case '3478025530':
-                rotopiaUsers.set(playing);
-                rotopiaVisits.set(visits);
-                rotopiaFav.set(favoritedCount);
-                rotopiaRating.set(fixedRatings);
-                break;
-            default:
-                break;
-        }
-    }
-}
+//                 await redis.set('play_crate_playing', playing);
+//                 await redis.set('play_crate_visits', visits);
+//                 productRobux.set(await returnSQL('product'));
+//                 gamepassRobux.set(await returnSQL('gamepass'));
+//                 break;
+//             case '701618141':
+//                 eatingUsers.set(playing);
+//                 eatingVisits.set(visits);
+//                 eatingFav.set(favoritedCount);
+//                 eatingRating.set(fixedRatings);
+//                 break;
+//             case '1730143810':
+//                 islandUser.set(playing);
+//                 islandVisit.set(visits);
+//                 islandFav.set(favoritedCount);
+//                 islandRating.set(fixedRatings);
+//                 break;
+//             case '3478025530':
+//                 rotopiaUsers.set(playing);
+//                 rotopiaVisits.set(visits);
+//                 rotopiaFav.set(favoritedCount);
+//                 rotopiaRating.set(fixedRatings);
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
+// }
 
-(async () => {
-    function CPU() {
-        const cpuUsage = process.cpuUsage();
-        const uptime = process.uptime() * 1000;
-        const cpuPercent = ((cpuUsage.user + cpuUsage.system) / uptime) * 100;
+// (async () => {
+//     function CPU() {
+//         const cpuUsage = process.cpuUsage();
+//         const uptime = process.uptime() * 1000;
+//         const cpuPercent = ((cpuUsage.user + cpuUsage.system) / uptime) * 100;
 
-        // Round the CPU usage percentage to two decimal places
-        return parseFloat(cpuPercent.toFixed(2));
-    }
+//         // Round the CPU usage percentage to two decimal places
+//         return parseFloat(cpuPercent.toFixed(2));
+//     }
 
-    function MEM() {
-        const used = process.memoryUsage().heapUsed;
-        const total = Math.max(process.memoryUsage().heapTotal, used);
-        return (used / total) * 100;
-    }
+//     function MEM() {
+//         const used = process.memoryUsage().heapUsed;
+//         const total = Math.max(process.memoryUsage().heapTotal, used);
+//         return (used / total) * 100;
+//     }
 
-    setInterval(() => {
-        CPUUsage.set(CPU());
-        MemoryUsage.set(MEM());
-    }, 1000);
+//     setInterval(() => {
+//         CPUUsage.set(CPU());
+//         MemoryUsage.set(MEM());
+//     }, 1000);
 
-    if (bot.Config.type === 'DEV') return;
+//     if (bot.Config.type === 'DEV') return;
 
-    try {
-        await Looper();
+//     try {
+//         await Looper();
 
-        setInterval(async () => {
-            await Looper();
-            console.log(`[Roblox API] Updated metrics!`);
-        }, bot.Config.server.refresh_time);
-    } catch (err) {
-        console.log(err);
-    }
-})();
+//         setInterval(async () => {
+//             await Looper();
+//             console.log(`[Roblox API] Updated metrics!`);
+//         }, bot.Config.server.refresh_time);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// })();
 
-for (const metrics of [
-    islandFav,
-    islandRating,
-    islandUser,
-    islandVisit,
-    rotopiaUsers,
-    rotopiaVisits,
-    rotopiaFav,
-    rotopiaRating,
-    eatingUsers,
-    eatingVisits,
-    eatingFav,
-    eatingRating,
-    bubbleUsers,
-    bubbleVisits,
-    bubbleFav,
-    bubbleRating,
-    productRobux,
-    gamepassRobux,
-    PlayCrateGroupCount,
-    MineCartGroupCount,
-    BreadedGroupCount,
-    StormyGroupCount,
-    CPUUsage,
-    MemoryUsage,
-]) {
-    register.registerMetric(metrics);
-}
+// for (const metrics of [
+//     islandFav,
+//     islandRating,
+//     islandUser,
+//     islandVisit,
+//     rotopiaUsers,
+//     rotopiaVisits,
+//     rotopiaFav,
+//     rotopiaRating,
+//     eatingUsers,
+//     eatingVisits,
+//     eatingFav,
+//     eatingRating,
+//     bubbleUsers,
+//     bubbleVisits,
+//     bubbleFav,
+//     bubbleRating,
+//     productRobux,
+//     gamepassRobux,
+//     PlayCrateGroupCount,
+//     MineCartGroupCount,
+//     BreadedGroupCount,
+//     StormyGroupCount,
+//     CPUUsage,
+//     MemoryUsage,
+// ]) {
+//     register.registerMetric(metrics);
+// }
 
-register.setDefaultLabels({
-    app: 'roblox',
-});
+// register.setDefaultLabels({
+//     app: 'roblox',
+// });
 
-module.exports = { register };
+// module.exports = { register };
